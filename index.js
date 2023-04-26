db0827024a8e8bb40175c3e0c6928ddb1bd4a8d5
Christopher Saunders <christopherallansaunders@gmail.com>
2023-04-26 16:28:17 -0500

    Added folders and file structure. Created routes.js.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -7,3 +8,5 @@ const port = 3080;
 app.listen(port, (req, res) => {
   console.log(`Server is runnuing on port ${port}.`);
 });
+
+app.use(bodyParser.json());
